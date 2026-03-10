54fcd63cdf7aae6e9a6ca120260aaba8b05b48a3
MubeenD1 <mubeen.dewan@gmail.com>
2026-03-10 22:49:03 +0200

    Added Ethan's curl message

--- api/status.js
+++ api/status.js
@@ -8,13 +8,36 @@ export default function handler(req, res) {
 
   res.setHeader('Content-Type', 'text/plain');
   res.status(200).send(`
-  ┌──────────────────────────────────────┐
-  │       ZENITH SYSTEMS — INTERNAL      │
-  └──────────────────────────────────────┘
+     ┌──────────────────────────────────────┐
+     │       ZENITH SYSTEMS — INTERNAL      │
+     └──────────────────────────────────────┘
 
-  [ Ethan's message here ]
+  If you're reading this then it worked! My name
+  is Ethan Larkin, I am apart of the recruitment
+  department of Zenith-Systems' Project Elyisum.
 
-  [ Maybe a URL to the admin portal at the end ]
+  I made that business card and hijacked the 
+  maintenance server because it's my only option 
+  to get people to know the truth.
+  
+  Project Elysium is not what it seems, I have
+  the clearance level to know what Zenith-Systems
+  is planning and they're lying to you!
+
+  I cannot send any documents outside of the
+  system's walls so I need you to take them from
+  me instead. 
+
+  Below, I have given you my admin credentials so 
+  you can see for yourself. People need to know,
+  this project cannot go through, for the sake
+  of all humanity... Please help
+  
+  zenith-systems.co.za/admin
+  user: e.larkin
+  pass: A17-KL3
+
+  – E.L
 
 `);
 }
